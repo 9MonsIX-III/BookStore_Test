f92c00aadeae98379050a87f01fb101dc8f1565e
9MonsIX-III <letuan250904@gmail.com>
2025-08-12 09:47:37 +0700

    test

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link từ React Router
 import './Header.css';
-import logo from "../../image/logo.png";
+import logo from '../../image/logo.png';
 
 function Header() {
   return (
     <header>
       <img className="logo" src={logo} alt="logo" />
-      
+
       <nav>
         <ul>
-          <li>Home</li>
-          <li>Books</li>
-          <li>...</li>
-          <li>...</li>
-          <li>...</li>
-          <li>...</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/books">Books</Link></li>
+          {/* Thay dấu chấm bằng các route cụ thể nếu cần */}
+          <li><Link to="/categories">Categories</Link></li> {/* Giả sử */}
+          <li><Link to="/deals">Deals</Link></li> {/* Giả sử */}
+          <li><Link to="/contact">Contact</Link></li> {/* Giả sử */}
+          <li><Link to="/about">About</Link></li> {/* Giả sử */}
         </ul>
       </nav>
 
       <input 
         type="text" 
-        placeholder="Tìm kiếm sách..." 
+        placeholder="Tìm kiếm sản phẩm..." 
         className="search-bar"
       />
 
-      <div className="checkout">Thanh toán</div>
+      <div className="checkout">
+        <Link to="/checkout">Thanh toán</Link> {/* Thêm Link cho Thanh toán */}
+      </div>
     </header>
   );
 }
